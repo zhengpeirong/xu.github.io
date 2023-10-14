2cf1c56603b832a3cd7d7b177d6256868609488c
zhengpeirong <1592635674@qq.com>
2023-10-15 06:05:21 +0800

    debug

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -1,30 +1,11 @@
-// var toggleButtons = document.getElementsByClassName('toggleButton');
-// var allAbstracts = document.getElementsByClassName('abstractContent');
-
-// for (var i = 0; i < toggleButtons.length; i++) {
-//     toggleButtons[i].addEventListener('click', function() {
-//         // 隐藏所有的 Abstract 内容，除开当前的Abstract
-//         for (var j = 0; j < allAbstracts.length; j++) {
-//             if (allAbstracts[j] !== event.target.nextElementSibling) {
-//                 allAbstracts[j].classList.add('hidden');
-//             }
-//         }
-//         // 显示与当前按钮相关的 Abstract 内容
-//         var content = event.target.nextElementSibling; // 获取紧随其后的元素（即要展示/隐藏的内容）
-//         // console.log(event.target); // 输出点击的按钮元素
-//         // console.log(event.target.nextElementSibling); // 输出点击的按钮元素
-//         content.classList.toggle('hidden');
-//         // if (allAbstracts[i] == event.target.nextElementSibling) {
-//         //     // 不管点击到谁，都会有反应，但是只改变点击到的那个，也就是i
-//         //     content.classList.toggle('hidden');
-//         // allAbstracts[i].classList.toggle('hidden');
-//     });
-// }
-
 var toggleButtons = document.getElementsByClassName('toggleButton');
 var allAbstracts = document.getElementsByClassName('abstractContent');
+var flag = false;
 
 function toggleAbstract(event) {
+    if (flag) return; // 如果标志为true，则直接返回，不执行函数
+    flag = true; // 第一次执行函数时设置标志为true
+
     for (var j = 0; j < allAbstracts.length; j++) {
         if (allAbstracts[j] !== event.target.nextElementSibling) {
             allAbstracts[j].classList.add('hidden');
@@ -32,15 +13,11 @@ function toggleAbstract(event) {
     }
     var content = event.target.nextElementSibling;
     content.classList.toggle('hidden');
-    return;
+
+    flag = false; // 在函数执行完毕后将标志重置为false
 
 }
 
 for (var i = 0; i < toggleButtons.length; i++) {
     toggleButtons[i].addEventListener('click', toggleAbstract);
 }
-
-
-
-
-
