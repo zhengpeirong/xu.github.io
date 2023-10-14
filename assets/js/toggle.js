44e95b65258f7974a905e7b4cb87dd59e4b60544
zhengpeirong <1592635674@qq.com>
2023-10-15 06:15:35 +0800

    debug

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -2,10 +2,6 @@ var toggleButtons = document.getElementsByClassName('toggleButton');
 var allAbstracts = document.getElementsByClassName('abstractContent');
 
 function toggleAbstract(event) {
-    if (event.target.hasAttribute('data-executed')) {
-        return; // 如果已经执行过函数，则直接返回，不再执行
-    }
-
     for (var j = 0; j < allAbstracts.length; j++) {
         if (allAbstracts[j] !== event.target.nextElementSibling) {
             allAbstracts[j].classList.add('hidden');
@@ -13,10 +9,14 @@ function toggleAbstract(event) {
     }
     var content = event.target.nextElementSibling;
     content.classList.toggle('hidden');
-
-    event.target.setAttribute('data-executed', 'true'); // 设置标志表示已经执行过函数
 }
 
-for (var i = 0; i < toggleButtons.length; i++) {
-    toggleButtons[i].addEventListener('click', toggleAbstract);
-}
+// for (var i = 0; i < toggleButtons.length; i++) {
+//     toggleButtons[i].addEventListener('click', toggleAbstract);
+// }
+
+document.addEventListener('click', function(event) {
+    if (event.target.classList.contains('toggleButton')) {
+        toggleAbstract(event);
+    }
+});
