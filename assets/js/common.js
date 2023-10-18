897c1f813451195b454f91d581f3c0bf0b9bccd2
zhengpeirong <1592635674@qq.com>
2023-10-18 17:23:08 +0800

    JavaScript增加不同期刊的颜色

--- assets/js/common.js
+++ assets/js/common.js
@@ -47,4 +47,49 @@ $(document).ready(function() {
     });
   });
   
-  
+
+
+/* 
+总共有以下11个期刊、会议的类别：
+  INFOCOM
+  CVPR
+  ICML
+  NIPS
+  IEEE WC
+  TWC
+  TMC
+  TPDS
+  TVT
+  TITS
+  JAS 
+*/
+
+document.addEventListener("DOMContentLoaded", function() {
+  // 获取所有abbr元素，分别分配颜色
+    let abbrElements = document.querySelectorAll('abbr.badge');
+
+    // 关键词和颜色的映射
+    const keywordColorMap = {
+        "INFOCOM": "#FF9999",
+        "CVPR": "#FFCC99",
+        "ICML": "#FFFF99",
+        "NIPS": "#CCFF99",
+        "IEEE WC": "#99FF99",
+        "TWC": "#99FFCC",
+        "TMC": "#99FFFF",
+        "TPDS": "#99CCFF",
+        "TVT": "#9999FF",
+        "TITS": "#CC99FF",
+        "JAS": "#FF99FF"
+    };
+
+    abbrElements.forEach(element => {
+        for (let keyword in keywordColorMap) {
+            if (element.textContent.includes(keyword)) {
+                element.style.backgroundColor = keywordColorMap[keyword];
+                element.style.color = "white";
+                break;  // 匹配到关键词后跳出循环
+            }
+        }
+    });
+});
